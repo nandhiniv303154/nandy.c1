33e96856145675c1438ffb27361423cb6c95fe94
nandhiniv303154 <45959798+nandhiniv303154@users.noreply.github.com>
2019-07-05 14:29:45 +0530

    Create set64.c

---
+++ set64.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int i,n;
+clrscr();
+printf("Enter the value:");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("%d",n);
+}
+else
+{
+printf("%d",i=n-1);
+}
+getch();
+}
